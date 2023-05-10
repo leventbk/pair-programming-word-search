72f3f3d3d8af527b2efd8ffe465e7d38a87d531f
leventkarakus <leventbkarakus@hotmail.com>
2023-05-10 17:00:00 -0400

    solution test

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,6 +4,14 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
+
+
+//What about the case where the word matrix is an empty array?
+it("should return error if the array is empty", function() {
+  const result = wordSearch([], 'FRANK')
+  assert.equal(result,'error1');
+});
+
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -32,6 +40,23 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  // What if the word is written vertically, not horizontally?
+  it("should return true if the word is present as Verticaly!", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASYHWBUOE')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if(letters.length===0){
+        return 'error1';
     }
+    const newArray=transpose(letters);
+    const newArrayJoin = newArray.map(ls=>ls.join(''));
+    console.log(newArrayJoin);
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    console.log(horizontalJoin);
+    const finalArray=newArrayJoin.concat(horizontalJoin);
+    //console.log(finalArray);
+    for (l of finalArray) {
+        if (l.includes(word)) return true
+    } // else if { veritacal test} else {return false}
+    return false;
 }
+const transpose = function (matrix) {
 
+    let arrayFinal = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      let array1 = [];
+      for (let j = 0; j < matrix.length; j++) {
+        array1.push(matrix[j][i]);
+      } arrayFinal.push(array1);
+    }
+    matrix = arrayFinal;
+    return matrix;
+  };
 module.exports = wordSearch
