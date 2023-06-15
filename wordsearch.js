623baecf262a017a920f60fe9db01ad4ea75cb30
Levent Karakus <43709259+leventbk@users.noreply.github.com>
2023-06-15 12:30:49 -0400

    Update wordsearch.js
    updated transpose functions

--- wordsearch.js
+++ wordsearch.js
@@ -1,29 +1,42 @@
-const wordSearch = (letters, word) => { 
-    if(letters.length===0){
-        return 'error1';
+const transpose = function (matrix) {
+    // Replace this code with your solution
+    
+    let result = [];
+  
+    for (let i = 0; i < matrix[0].length; i++) {
+      result.push([]);
     }
-    const newArray=transpose(letters);
-    const newArrayJoin = newArray.map(ls=>ls.join(''));
-    console.log(newArrayJoin);
-    const horizontalJoin = letters.map(ls => ls.join(''));
-    console.log(horizontalJoin);
-    const finalArray=newArrayJoin.concat(horizontalJoin);
-    //console.log(finalArray);
-    for (l of finalArray) {
+  
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        result[j].push(matrix[i][j])
+      }
+  
+    }
+  
+    return result;
+  };
+
+
+const wordSearch = (letters, word) => { 
+    if (letters.length === 0) {
+        return 'error';
+    }
+    if (letters[0].length === 0) {
+        return 'error';
+    }
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    for (l of horizontalJoin) {
         if (l.includes(word)) return true
-    } // else if { veritacal test} else {return false}
+    }
+
+    const arrVertical = transpose(letters);
+    const verticalJoin = arrVertical.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+
     return false;
 }
-const transpose = function (matrix) {
 
-    let arrayFinal = [];
-    for (let i = 0; i < matrix[0].length; i++) {
-      let array1 = [];
-      for (let j = 0; j < matrix.length; j++) {
-        array1.push(matrix[j][i]);
-      } arrayFinal.push(array1);
-    }
-    matrix = arrayFinal;
-    return matrix;
-  };
 module.exports = wordSearch
